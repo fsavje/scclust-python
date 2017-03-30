ca52da6f97955bcb48accc8ada7b09c5aa678573
RoseZhao <rosezhao@berkeley.edu>
2017-03-30 14:03:04 -0700

    Update input_check.py

--- input_check.py
+++ input_check.py
@@ -1,3 +1,5 @@
+import pyximport; pyximport.install()
+
 # ==============================================================================
 # Helper functions
 # ==============================================================================
@@ -17,7 +19,7 @@ def ensure_distances(distances, req_length = None):
 		if (!is_distances(d)):
 			new_error(distances, "is not a 'scc_distances' object.")
 	if (req_length != None and len(distances) != req_length):
-		new_error(distances, "does not contain " + str(req_length))
+		new_error(distances, "does not contain " + str(req_length) + "` data points.")
 
 
 # Ensure that `indicators` are non-NA logicals
@@ -30,3 +32,27 @@ def ensure_indicators(indicators, req_length = None, any_true = False):
 			new_error(indicators, "may not contain null.")
 	if (req_length != None & (len(indicators) != req_length)):
 		new_error(indicators, "is not of length " + str(req_length))
+	if (any_true):
+		if(!any(indicators)):
+			new_error(indicators, "cannot be all `FALSE`.")
+
+
+# Ensure that `clustering` is a `scclust` object
+
+def ensure_scclust(clustering, req_length = None):
+	if(!is_scclust(clustering)):
+		new_error(clustering, "` is not a `scclust` object.")
+	if(req_length != None & len(clustering) != req_length):
+		new_error(clustering, " does not contain " + str(req_length) + "` data points.")
+
+# ==============================================================================
+# Coerce functions
+# ==============================================================================
+
+
+# Coerce `x` to character vector
+def coerce_character(int x, req_length = None):
+	cdef char* x_c
+	itoa(x, x_c,10)
+	if(req_length != None & len(x_c) != req_length):
+		new_error(x, "is not of length " + str(req_length))
